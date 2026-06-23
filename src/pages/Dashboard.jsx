7cb04b4854ce80719b48ca55c47c3a2224e3ab19
Abhinav <upadhyayshobhawati98@gmail.com>
2026-06-23 15:22:22 +0530

    Update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -101,6 +101,7 @@ function Dashboard() {
 	    const response = await axios.get("https://nexora-backend-yo2e.onrender.com/api/ai/chat", {
 	      withCredentials: true
 	    })
+	    alert(reponse.data.chats)
 	    setChats(response.data.chats)
 	  } catch (err) {
 	    console.log(err)
